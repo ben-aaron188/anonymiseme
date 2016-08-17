84d6e7656ce8cfb01d3b0df0a1d3d21c9f883b20
ben-aaron188 <bennettkleinberg@BAR-MacBook-Air.fritz.box>
2016-08-17 19:14:11 +0200

    init barebone structure

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// simpel architecture
+
+// input
+//  formats txt, doc(x)
+
+// processing
+//  get entities for anonymisation
+//  re-phrase entities
+
+
+// output
+//  write new downloadable text file
