5e18205c7825d429f41c26ddd30a95b5c05ef353
ben-aaron188 <bennettkleinberg@re-staff-145-18-22-51.wireless.uva.nl>
2016-08-23 18:13:34 +0200

    min fix

--- js/nlp_replacer.js
+++ js/nlp_replacer.js
@@ -4,67 +4,66 @@
 var temp_replacers = [];
 var replaced_arr = [];
 
-function replace1_single(preprocessed_object, entity) {
-    var span_wrap = [];
-    var preprocessed_object2 = set_replacers(preprocessed_object);
-    switch (entity) {
-        case 'persons':
-            $(preprocessed_object2.terms).each(function(i, el) {
-                if (el.pos.Person) {
-                    span_wrap.push('<span class="highlighted persons">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
-                } else {
-                    span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
-                }
-            });
-            break;
-        case 'locations':
-            $(preprocessed_object.terms).each(function(i, el) {
-                if (el.pos.Place) {
-                    span_wrap.push('<span class="highlighted locations">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
-                } else {
-                    span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
-                }
-            });
-            break;
-        case 'organizations':
-            $(preprocessed_object.terms).each(function(i, el) {
-                if (el.pos.Organization) {
-                    span_wrap.push('<span class="highlighted organizations">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
-                } else {
-                    span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
-                }
-            });
-            break;
-        case 'dates':
-            $(preprocessed_object.terms).each(function(i, el) {
-                if (el.pos.Date) {
-                    span_wrap.push('<span class="highlighted dates">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
-                } else {
-                    span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
-                }
-            });
-            break;
-        case 'values':
-            $(preprocessed_object.terms).each(function(i, el) {
-                if (el.pos.Value || el.pos.Currency) {
-                    span_wrap.push('<span class="highlighted values">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
-                } else {
-                    span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
-                }
-            });
-            break;
-    }
-    $('#ex3').text('');
-    $(span_wrap).each(function(i, el) {
-        $('#ex3').append(el);
-        // change to replacer
-    });
-}
+// function replace1_single(preprocessed_object, entity) {
+//     var span_wrap = [];
+//     var preprocessed_object2 = set_replacers(preprocessed_object);
+//     switch (entity) {
+//         case 'persons':
+//             $(preprocessed_object2.terms).each(function(i, el) {
+//                 if (el.pos.Person) {
+//                     span_wrap.push('<span class="highlighted persons">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
+//                 } else {
+//                     span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
+//                 }
+//             });
+//             break;
+//         case 'locations':
+//             $(preprocessed_object.terms).each(function(i, el) {
+//                 if (el.pos.Place) {
+//                     span_wrap.push('<span class="highlighted locations">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
+//                 } else {
+//                     span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
+//                 }
+//             });
+//             break;
+//         case 'organizations':
+//             $(preprocessed_object.terms).each(function(i, el) {
+//                 if (el.pos.Organization) {
+//                     span_wrap.push('<span class="highlighted organizations">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
+//                 } else {
+//                     span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
+//                 }
+//             });
+//             break;
+//         case 'dates':
+//             $(preprocessed_object.terms).each(function(i, el) {
+//                 if (el.pos.Date) {
+//                     span_wrap.push('<span class="highlighted dates">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
+//                 } else {
+//                     span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
+//                 }
+//             });
+//             break;
+//         case 'values':
+//             $(preprocessed_object.terms).each(function(i, el) {
+//                 if (el.pos.Value || el.pos.Currency) {
+//                     span_wrap.push('<span class="highlighted values">' + el.whitespace.preceding + el.replacement + el.whitespace.trailing + '</span>');
+//                 } else {
+//                     span_wrap.push('<span class="non_highlighted">' + el.whitespace.preceding + el.text + el.whitespace.trailing + '</span>');
+//                 }
+//             });
+//             break;
+//     }
+//     $('#ex3').text('');
+//     $(span_wrap).each(function(i, el) {
+//         $('#ex3').append(el);
+//         // change to replacer
+//     });
+// }
 
 // repetitions
 function replace1_all(preprocessed_object) {
     var span_wrap = [];
-    // var preprocessed_object2 = set_replacers(preprocessed_object);
     $(preprocessed_object.terms).each(function(i, el) {
         if (el.pos.Person && el.pos.Pronoun !== true) {
             define_replacement(el);
