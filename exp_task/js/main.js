ac0989b9818f3432b3753f3637484ea39e964aa4
ben-aaron188 <bennettkleinberg@BAR-MacBook-Air.fritz.box>
2016-09-16 22:00:54 +0200

    adj feedback for examples; added cond. unid assignment; small fixes

--- exp_task/js/main.js
+++ exp_task/js/main.js
@@ -1,6 +1,4 @@
 // globals
-var time_langtask1 = 90000;
-var time_instr = 30000;
 var data_array = [];
 var data_statement1;
 var data_statement2;
@@ -8,32 +6,17 @@ var data_statement3;
 var data_statement4;
 var data_example1;
 var data_example2;
+var unid;
+var validation_score = 0;
 
 // task flow
-$(document).ready(function () {
-    unid = twoletters() + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9);
+$(document).ready(function() {
     init_data();
     getIP();
     $("#intro1").show();
     $("#next").attr('onclick', 'to_informed_consent()');
-    cond1 = 2; // veracity
-    cond2 = randomdigit(0, 1); //cue
-    // cond2 = 1;
-    cond3 = 2; //temporality
-    text_validation = false;
 });
 
-
-function get_content(category) {
-    var index = Math.floor(Math.random() * data_statements.length);
-    var statement = data_statements[index];
-
-    if (statement.category != category) {
-        return get_content(category);
-    }
-
-    return statement;
-}
 
 function to_informed_consent() {
     simple_transition($("#intro1"), $("#informed_consent"));
@@ -44,9 +27,9 @@ function to_statement_evaluation() {
     $('body').prepend('<div id="statement_explanation">' +
         'Explanation</br>' +
         'You will now first read two example statements to test whether you correctly understood the definitions we want you to apply to the main statements.</br>' +
-        'Please adhere to the following two definitions of plausibility and detailedness.</br>' +
-        'Plausibility = the coherency of the statement in terms of not containing logical inconsistencies or contradictions.</br>' +
-        'Detailedness = the inclusion of specific descriptions of place, time, persons, objects and events.</br>' +
+        'Please adhere to the following two definitions of plausibility and detailedness.</br></br>' +
+        'Plausibility = the coherency of the statement in terms of not containing logical inconsistencies or contradictions.</br></br>' +
+        'Detailedness = the inclusion of specific descriptions of place, time, persons, objects and events.</br></br>' +
         'After the two examples, you get feedback and will then proceed to the four main statements.</br>' +
         '</div>'
     );
@@ -56,26 +39,24 @@ function to_statement_evaluation() {
 }
 
 function set_plaus_slider_value(number) {
-    var input = "statement" + number + "_evaluation_plausibility_value";
-    var output = "plaus_output_" + number;
-
-    document.getElementById(output).value = document.getElementById(input).value;
+    var input = "#statement" + number + "_evaluation_plausibility_value";
+    var output = "#plaus_output_" + number;
+    $(output).val($(input).val());
 }
 
 function set_vivid_slider_value(number) {
-    var input = "statement" + number + "_evaluation_vividness_value";
-    var output = "vivid_output_" + number;
-
-    document.getElementById(output).value = document.getElementById(input).value;
+    var input = "#statement" + number + "_evaluation_vividness_value";
+    var output = "#vivid_output_" + number;
+    $(output).val($(input).val());
 }
 
 function add_statement(number, example) {
+    var header;
     if (!example) {
-        var header = "Statement " + number + "/4";
+        header = "Statement " + number + "/4";
     } else {
-        var header = example;
+        header = example;
     }
-
     $('body').prepend(
         '<div class="statement_wrapper" id="statement' + number + '_wrapper">' +
         '<div class="statement_header" id="statement' + number + '_header">' + header + '</div>' +
@@ -103,18 +84,21 @@ function add_statement(number, example) {
 
 function to_example1() {
     data_example1 = examples[0];
-
     add_statement(5, "Example 1");
-
     $("#statement5_content").text(data_example1.content);
     simple_transition($("#statement_explanation"), $("#statement5_wrapper"));
-
-    $("#next").attr('onclick', 'to_example2()');
+    $("#next").attr('onclick', 'verify_example_1()');
 }
 
 function to_example2() {
     data_example2 = examples[1];
+    add_statement(6, "Example 2");
+    $("#statement6_content").text(data_example2.content);
+    simple_transition($("#statement5_wrapper"), $("#statement6_wrapper"));
+    $("#next").attr('onclick', 'verify_example_2()');
+}
 
+function verify_example_1() {
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_example1,
@@ -122,17 +106,12 @@ function to_example2() {
                 plausibility: $("#statement5_evaluation_plausibility_value").val(),
                 vividness: $("#statement5_evaluation_vividness_value").val(),
             }
-        })
-
-        add_statement(6, "Example 2");
-
-        $("#statement6_content").text(data_example2.content);
-        simple_transition($("#statement5_wrapper"), $("#statement6_wrapper"));
-        $("#next").attr('onclick', 'verify_examples()');
+        });
+        check_example_validation('example1');
     }
 }
 
-function verify_examples() {
+function verify_example_2() {
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_example2,
@@ -141,80 +120,87 @@ function verify_examples() {
                 vividness: $("#statement6_evaluation_vividness_value").val(),
             }
         });
-
-        check_example_validation();
+        check_example_validation('example2');
     }
-
 }
 
-function check_example_validation() {
-    var expected = 8;
 
+function check_example_validation(example_number) {
+    var vivid_expected_example1 = 8;
+    var plaus_expected_example1 = 8;
+    var vivid_expected_example2 = 8;
+    var plaus_expected_example2 = 8;
     var plaus_example1 = $("#statement5_evaluation_plausibility_value").val();
     var vivid_example1 = $("#statement5_evaluation_vividness_value").val();
     var plaus_example2 = $("#statement6_evaluation_plausibility_value").val();
     var vivid_example2 = $("#statement6_evaluation_vividness_value").val();
-
-    if (Math.abs(plaus_example1 - expected) > 2 ||
-        Math.abs(vivid_example1 - expected) > 2 ||
-        Math.abs(plaus_example2 - expected) > 2 ||
-        Math.abs(vivid_example2 - expected) > 2
-    ) {
-        wrong_evaluation();
-    } else {
-        well_done();
+    switch (example_number) {
+        case 'example1':
+            if (Math.abs(plaus_example1 - plaus_expected_example1) > 2 ||
+                Math.abs(vivid_example1 - vivid_expected_example1) > 2
+            ) {
+                validation_score++;
+                wrong_evaluation(example_number, plaus_expected_example1, vivid_expected_example1);
+            } else {
+                well_done(example_number);
+            }
+            break;
+        case 'example2':
+            if (Math.abs(plaus_example2 - plaus_expected_example2) > 2 ||
+                Math.abs(vivid_example2 - vivid_expected_example2) > 2
+            ) {
+                validation_score++;
+                wrong_evaluation(example_number, plaus_expected_example2, vivid_expected_example2);
+            } else {
+                well_done(example_number);
+            }
+            break;
+    }
+    switch (example_number) {
+        case 'example1':
+            $("#next").attr('onclick', 'to_example2()');
+            break;
+        case 'example2':
+            $("#next").attr('onclick', 'to_main_statements()');
+            break;
     }
 }
 
-function wrong_evaluation() {
-    $('body').prepend(
-        '<div id="show_example_validation">' +
-        'Your evaluation was not like we expected it to be.<br>' +
-        'For both examples, we expected a plausibility of 8 and a vividness of 8.<br>' +
-        '<hr/>' +
-        'You can now start with the main task!<br>' +
-        '</div>'
-    );
-
-    simple_transition($("#statement6_wrapper"), $("#show_example_validation"));
-    $("#next").attr('onclick', 'to_main_statements()');
+function wrong_evaluation(example_number, exp_plaus, exp_vivid) {
+    var message = "Your evaluation was not correct. For this example, we expected a plausibility rating of " + exp_plaus + " and a detailedness rating of " + exp_vivid + ".";
+    alert(message);
 }
 
-function well_done() {
-    $('body').prepend(
-        '<div id="well_done">' +
-        'Well done!<br>' +
-        'Your evaluation was just as we expected it to be.<br>' +
-        '<hr/>' +
-        'You can now start with the main task.<br>' +
-        '</div>'
-    );
-
-    simple_transition($("#statement6_wrapper"), $("#well_done"));
-    $("#next").attr('onclick', 'to_main_statements()');
+function well_done(example_number) {
+    var message;
+    switch (example_number) {
+        case 'example1':
+            message = "Well done. Your evaluation was just as expected. You can now proceed to the second example.";
+            break;
+        case 'example2':
+            message = "Well done. Your evaluation was just as expected. You can now proceed to the proper task.";
+            break;
+    }
+    alert(message);
 }
 
 function to_main_statements() {
-    $("#show_example_validation").css('display', 'none');
-    $("#well_done").css('display', 'none');
-
+    get_unid(validation_score);
+    // $("#show_example_validation").css('display', 'none');
+    // $("#well_done").css('display', 'none');
     to_statement1();
 }
 
 function to_statement1() {
     data_statement1 = get_content(1);
-
     add_statement(1, null);
-
     $("#statement1_content").text(data_statement1.content);
     simple_transition($("#statement6_wrapper"), $("#statement1_wrapper"));
-
     $("#next").attr('onclick', 'to_statement2()');
 }
 
 function to_statement2(prev_data) {
     data_statement2 = get_content(2);
-
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_statement1,
@@ -222,20 +208,16 @@ function to_statement2(prev_data) {
                 plausibility: $("#statement1_evaluation_plausibility_value").val(),
                 vividness: $("#statement1_evaluation_vividness_value").val(),
             }
-        })
-
+        });
         add_statement(2, null);
-
         $("#statement2_content").text(data_statement2.content);
         simple_transition($("#statement1_wrapper"), $("#statement2_wrapper"));
-
         $("#next").attr('onclick', "to_statement3('data')");
     }
 }
 
 function to_statement3(prev_data) {
     data_statement3 = get_content(3);
-
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_statement2,
@@ -243,9 +225,8 @@ function to_statement3(prev_data) {
                 plausibility: $("#statement2_evaluation_plausibility_value").val(),
                 vividness: $("#statement2_evaluation_vividness_value").val(),
             }
-        })
+        });
         add_statement(3, null);
-
         $("#statement3_content").text(data_statement3.content);
         simple_transition($("#statement2_wrapper"), $("#statement3_wrapper"));
         $("#next").attr('onclick', "to_statement4('data')");
@@ -254,7 +235,6 @@ function to_statement3(prev_data) {
 
 function to_statement4(prev_data) {
     data_statement4 = get_content(4);
-
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_statement3,
@@ -262,9 +242,8 @@ function to_statement4(prev_data) {
                 plausibility: $("#statement3_evaluation_plausibility_value").val(),
                 vividness: $("#statement3_evaluation_vividness_value").val(),
             }
-        })
+        });
         add_statement(4, null);
-
         $("#statement4_content").text(data_statement4.content);
         simple_transition($("#statement3_wrapper"), $("#statement4_wrapper"));
         $("#next").attr('onclick', "to_transition(data)");
@@ -272,7 +251,6 @@ function to_statement4(prev_data) {
 }
 
 function to_transition(prev_data) {
-
     if (check_slider($(".slider_io_output"))) {
         data_array.push({
             content: data_statement4,
@@ -284,7 +262,6 @@ function to_transition(prev_data) {
         simple_transition($("#statement4_wrapper"), $("#transition1"));
         $("#next").attr('onclick', 'to_demographics1()');
     }
-
 }
 
 function to_demographics1() {
@@ -308,7 +285,6 @@ function to_outro() {
         simple_transition($("#demographics2"), $("#outro"));
         $("#partcode").text(unid);
         // $("#next").show();
-
         $("#next").text('SEND');
         $("#next").attr('onclick', 'send_to_server()');
     }
@@ -346,6 +322,14 @@ function get_data() {
     data.example2_eval_plausibility = data_array[1].evaluation.plausibility;
     data.example2_eval_vividness = data_array[1].evaluation.vividness;
 
+    data.statement1_category = data_array[2].content.category;
+    data.statement1_content = data_array[2].content.content;
+    data.statement1_plausibility = data_array[2].content.plausibility;
+    data.statement1_vividness = data_array[2].content.vividness;
+    data.statement1_type = data_array[2].content.type;
+    data.statement1_eval_plausibility = data_array[2].evaluation.plausibility;
+    data.statement1_eval_vividness = data_array[2].evaluation.vividness;
+
     data.statement2_category = data_array[3].content.category;
     data.statement2_content = data_array[3].content.content;
     data.statement2_plausibility = data_array[3].content.plausibility;
@@ -369,6 +353,4 @@ function get_data() {
     data.statement4_type = data_array[5].content.type;
     data.statement4_eval_plausibility = data_array[5].evaluation.plausibility;
     data.statement4_eval_vividness = data_array[5].evaluation.vividness;
-
 }
-
--- exp_task/js/utils.js
+++ exp_task/js/utils.js
@@ -3,10 +3,10 @@ var slider_moved_array = [];
 function check_fields(classname) {
     class_values = [];
     score = 0;
-    classname.each(function () {
+    classname.each(function() {
         if ($(this).is(":visible")) {
             // if($(this).attr('type') == 'text'){
-            $(this).each(function () {
+            $(this).each(function() {
                 class_values.push($(this).val().length);
                 score = $.inArray(0, class_values);
             });
@@ -31,14 +31,14 @@ function has_second_language() {
 
 function define_keys(ID, allowedInput, allowedMax) {
     if (allowedInput == "number") {
-        ID.keypress(function (e) {
+        ID.keypress(function(e) {
             var code = e.keyCode || e.which;
             if (code != 8 && code !== 0 && (code < 48 || code > 57)) {
                 return false;
             }
         });
     } else if (allowedInput == "text") {
-        ID.keypress(function (e) {
+        ID.keypress(function(e) {
             var code = e.keyCode || e.which;
             if (code > 32 && (code < 65 || code > 90) &&
                 (code < 97 || code > 122)) {
@@ -76,173 +76,12 @@ function shuffle(array) {
     return newarr;
 }
 
-var now = (function () {
-    var performance = window.performance || {};
-    performance.now = (function () {
-        return performance.now ||
-            performance.webkitNow ||
-            performance.msNow ||
-            performance.oNow ||
-            performance.mozNow ||
-            function () {
-                return new Date().getTime();
-            };
-    })();
-    return performance.now();
-});
-
-function validate_text(ID, desiredLength, test_kind) {
-    // text_validation = false;
-    if (test_kind == 'both') {
-        if (check_text(ID, desiredLength) === true) {
-            if (check_input(ID) === true) {
-                text_validation = true;
-                // alert('text validated');
-                return true;
-            }
-        }
-    } else if (test_kind == 'length') {
-        if (check_text(ID, desiredLength) === true) {
-            text_validation = true;
-            // alert('text validated');
-            return true;
-        }
-    } else if (test_kind == 'content') {
-        if (check_input(ID) === true) {
-            text_validation = true;
-            // alert('text validated');
-            return true;
-        }
-    }
-}
-
-function check_input(ID) {
-    keywords = ["the", "to", "in", "at", "with", "by", "of", "a", "an", "from", "on", "there", "here", "I", "you", "they", "we", "us", "is", "it"];
-    tester_array = [];
-    tester2 = 0;
-    textin = ID.val().toLowerCase().split(" ");
-    $.each(keywords, function (index, val) {
-        tester = $.inArray(val, textin);
-        if (tester < 0) {
-            tester2 = 0;
-        } else {
-            tester2 = 1;
-        }
-        tester_array.push(tester2);
-    });
-    checksum_tester = sum(tester_array);
-    if ((checksum_tester / textin.length) < 0.025) {
-        alert("Please use real English words and sentences in your answer. You will not be able to proceed otherwise. We cannot validate your participation without serious participation.");
-    } else {
-        return true;
-    }
-}
-
-function check_text(ID, desiredLength) {
-    var raw = ID.val().toLowerCase().replace(/ /g, '');
-    if (raw.length < desiredLength) {
-        alert("Please use at least " + desiredLength + " characters to answer this questions.");
-    } else {
-        return true;
-    }
-}
-
-function sum(arr) {
-    var r = 0;
-    $.each(arr, function (i, v) {
-        r += v;
-    });
-    return r;
-}
-
-function record_elapsed_start(ID) {
-    elapsed = 0;
-    ID.keypress(function (e) {
-        time1 = now();
-    });
-}
-
-function record_elapsed_end(ID) {
-    time2 = now();
-    elapsed = time2 - time1;
-}
-
-
-function get_length(ID) {
-    lengthtext = ID.val().toLowerCase().split(" ");
-    id_length = lengthtext.length;
-    // return id_length;
-}
-
-function record_deletes(ID) {
-    var listen = true;
-    deletions_arr = [];
-    ID.keydown(function (e) {
-        if (listen === true) {
-            var code = e.keyCode || e.which;
-            if (code == 8) {
-                deletions_arr.push(1);
-            }
-        }
-    });
-}
-
-function record_gaps(ID) {
-    var listen = true;
-    elapsed_arr = [];
-    var t0 = now();
-    ID.keydown(function (e) {
-        if (listen === true) {
-            var t1 = now();
-            var elapsed = t1 - t0;
-            elapsed_arr.push(elapsed.toFixed(2));
-            t0 = now();
-        }
-    });
-}
-
-function q_meta_init(ID) {
-    // elapsed
-    record_elapsed_start(ID);
-    // deletions
-    record_deletes(ID);
-    // interruptions
-    record_gaps(ID);
-}
-
-function q_meta_retrieve(ID, abbrev_q_code) {
-    // length
-    get_length(ID);
-    // elapsed
-    record_elapsed_end(ID);
-
-    var length_proxy = abbrev_q_code + '_length';
-    var elapsed_proxy = abbrev_q_code + '_elapsed';
-    var deletions_proxy = abbrev_q_code + '_deletions';
-    var gaps_proxy_100 = abbrev_q_code + '_gaps_100';
-    var gaps_proxy_200 = abbrev_q_code + '_gaps_200';
-    var gaps_proxy_300 = abbrev_q_code + '_gaps_300';
-
-    window[length_proxy] = id_length;
-    window[elapsed_proxy] = elapsed.toFixed(2);
-    window[deletions_proxy] = deletions_arr.length;
-    window[gaps_proxy_100] = elapsed_arr.filter(function (x) {
-        return x > 100;
-    }).length;
-    window[gaps_proxy_200] = elapsed_arr.filter(function (x) {
-        return x > 200;
-    }).length;
-    window[gaps_proxy_300] = elapsed_arr.filter(function (x) {
-        return x > 300;
-    }).length;
-}
-
 function init_data() {
     data = {};
 }
 
 function simple_transition(current_div, next_div) {
-    current_div.hide(function () {
+    current_div.hide(function() {
         next_div.show();
     });
 }
@@ -256,30 +95,41 @@ function send_to_server() {
 }
 
 function getIP() {
-    $.get("http://ipinfo.io", function (response) {
+    $.get("http://ipinfo.io", function(response) {
         window.clientip = response.ip;
     }, "jsonp");
-}
-
-function capitalise_string(stringinput) {
-    var string_old = stringinput;
-    var new_string = string_old[0].toUpperCase() + string_old.slice(1).toLowerCase();
-    return new_string;
 }
 
 function check_slider(classname) {
     class_values = [];
     score = 0;
-    classname.each(function () {
+    classname.each(function() {
         if ($(this).is(":visible")) {
             class_values.push($(this).val().length);
             score = $.inArray(15, class_values);
         }
     });
     if (score != -1) {
-        alert("Please move the sliders to give your answers.");
+        alert("Please move both sliders to give your answers.");
         score = 0;
     } else {
         return true;
+    }
+}
+
+function get_content(category) {
+    var index = Math.floor(Math.random() * data_statements.length);
+    var statement = data_statements[index];
+    if (statement.category != category) {
+        return get_content(category);
+    }
+    return statement;
+}
+
+function get_unid(val_score) {
+    if (val_score === 0) {
+        unid = twoletters() + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9);
+    } else {
+        unid = twoletters() + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9) + randomdigit(0, 9) + "_X";
     }
 }
